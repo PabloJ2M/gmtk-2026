8e596bc9e7155f3e3b3e4f98e8e67f059675977a
Pablo Mansilla Martinelli <pablomansilla60@gmail.com>
2026-07-24 08:49:47 -0500

    fixed platform parent interaction

--- Assets/_ProjectFolder/Code/Scripts/Platforms/Platform.cs
+++ Assets/_ProjectFolder/Code/Scripts/Platforms/Platform.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other) =>
-        other.transform.SetParent(transform);
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.contacts[0].normal.y < -0.5f)
+            other.transform.SetParent(transform);
+    }
     
     private void OnCollisionExit2D(Collision2D other) =>
         other.transform.SetParent(null);
